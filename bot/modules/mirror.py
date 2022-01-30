29c4d0a5f0bf913e511b07309da97a1d4a7ee689
ratnamraju <89572624+ratnamraju@users.noreply.github.com>
2022-01-30 13:59:39 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -252,7 +252,7 @@ class MirrorListener:
                     share_url = short_url(share_url)
                     buttons.buildbutton("⚡ Index Link", share_url)
                     if VIEW_LINK:
-                        share_urls = f'{INDEX_URL}/{url_path}?a=view'
+                        share_urls = f'{INDEX_URL}/{url_path}#Intent;package=com.mxtech.videoplayer.ad;S.title=@TroopOriginals;end'
                         share_urls = short_url(share_urls)
                         buttons.buildbutton("🌐 View Link", share_urls)
             if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
